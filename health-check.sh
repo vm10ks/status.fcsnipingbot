650783d1d1ae9826d8511740f042d120df0e316f
vm10k <129557697+vm10k@users.noreply.github.com>
2023-12-18 22:26:34 +0200

    Update health-check.sh

--- health-check.sh
+++ health-check.sh
@@ -1,3 +1,6 @@
+# In the original repository we'll just print the result of status checks,
+# without committing. This avoids generating several commits that would make
+# later upstream merges messy for anyone who forked us.
 commit=true
 origin=$(git remote get-url origin)
 if [[ $origin == *statsig-io/statuspage* ]]
@@ -52,8 +55,11 @@ do
     echo "    $dateTime, $result"
   fi
 done
+
 if [[ $commit == true ]]
 then
+  git config --global user.name 'Vijaye Raji'
+  git config --global user.email 'vijaye@statsig.com'
   git add -A --force logs/
   git commit -am '[Automated] Update Health Check Logs'
   git push
