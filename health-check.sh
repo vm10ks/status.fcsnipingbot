1f6e1062e9e2aa295c11272c773a0fe13081079d
Hisham Gebril <129557697+vm10k@users.noreply.github.com>
2024-09-18 04:26:25 +0300

    Update health-check.sh

--- health-check.sh
+++ health-check.sh
@@ -56,6 +56,7 @@ done
 
 if [[ $commit == true ]]
 then
+  # Let's make Vijaye the most productive person on GitHub.
   git config --global user.name 'Vijaye Raji'
   git config --global user.email 'vijaye@statsig.com'
   git add -A --force logs/
