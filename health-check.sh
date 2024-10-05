a74f48dde0ed4835b412dc3ea7c41f253b2f7eee
vm10ks <140952752+vm10ks@users.noreply.github.com>
2024-10-05 05:51:29 +0300

    Update health-check.sh

--- health-check.sh
+++ health-check.sh
@@ -1,4 +1,3 @@
-
 commit=true
 origin=$(git remote get-url origin)
 if [[ $origin == *statsig-io/statuspage* ]]
@@ -30,6 +29,9 @@ do
   url="${URLSARRAY[index]}"
   echo "  $key=$url"
 
+  # Perform ping check and record the ping time
+  pingTime=$(ping -c 1 -q $(echo $url | awk -F/ '{print $3}') | awk -F'/' 'END{ print (/^rtt/? $5" ms":"timeout") }')
+
   for i in 1 2 3 4; 
   do
     response=$(curl --write-out '%{http_code}' --silent -A  'Mozilla/5.0 Chrome/120' --output /dev/null $url)
@@ -43,14 +45,17 @@ do
     fi
     sleep 5
   done
+
   dateTime=$(date +'%Y-%m-%d %H:%M')
+
+  # Log both HTTP status and ping time
   if [[ $commit == true ]]
   then
-    echo $dateTime, $result >> "logs/${key}_report.log"
-    # By default we keep 2000 last log entries.  Feel free to modify this to meet your needs.
+    echo "$dateTime, $result, Ping: $pingTime" >> "logs/${key}_report.log"
+    # By default, we keep 2000 last log entries.  Feel free to modify this to meet your needs.
     echo "$(tail -2000 logs/${key}_report.log)" > "logs/${key}_report.log"
   else
-    echo "    $dateTime, $result"
+    echo "    $dateTime, $result, Ping: $pingTime"
   fi
 done
 
